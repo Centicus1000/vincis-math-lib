52b2053724ed7983ee64b0c58c96da61fc6955ba
Centicus1000 <55696718+Centicus1000@users.noreply.github.com>
2022-05-29 20:49:40 +0200

    Vec4 -> English Doku

--- vml/src/vec4.cpp
+++ vml/src/vec4.cpp
@@ -3,85 +3,101 @@
 using namespace vml;
 
 // ----------------------------------------------
-// Konstruktoren
+// Constructors
 
 /**
- * @brief Standard Konstruktor.
+ * @brief Standard Constructor.
  *
- * Initialisiert explizit die vier Koordinaten des Vektors.
+ * Initializes the four coordinates explicitly via parameter input. The order of the parameter corresponds to x, y, z, w.
  */
 vec4::vec4(float _x, float _y, float _z, float _w) :
 x(_x), y(_y), z(_z), w(_w)
 {}
 
 /**
- * @brief Konstruktor mit nur einer Zahl.
+ * @brief Single Number Constructor.
  *
- * Initialisiert alle vier Koordinaten mit dem selben Wert.
+ * Initializes all four attributes to the same value which is supplied via parameter input. This calls the Standard Constructor.
  *
- * @param all der Wert von allen Koordinaten
+ * @param all the value that all attributes are set to
  */
 vec4::vec4(float all) : vec4(all, all, all, all) {}
 
-/**
- * @brief Array-Konstruktor.
- *
- * Ermöglicht das initialisieren des Vektors über einen std::array
- */
-vec4::vec4(std::array<float, size> data) :
-vec4(data[0], data[1], data[2], data[3])
-{}
-
 
 /**
- * @brief Nullvektor (Default Konstruktor)
+ * @brief Zero Element  (Default Constructor)
  *
- * Initialisiert alle vier Koordinaten auf 0.
+ * Initializes all four attributes to the default value of zero. This calls the Standard Constructor.
  */
 vec4::vec4() : vec4(0.f, 0.f, 0.f, 0.f)
 {}
 
 
 // ----------------------------------------------
-// Operatoren
+// Operators
 
 /**
- * @name Mutierende Operatoren
+ * @brief Inversion.
  *
- * Diese Funktionen veränderen die Daten des Vec2.
+ * Applying the non-binary minus operator, the signs of all attributes is toggled. Positives become negative and vice versa.
  */
-///@{
-/** Invertierung */
 vec4 vec4::operator -() const
 {
     return vec4(-x, -y, -z, -w);
 }
-/** Addition */
+
+/**
+ * @brief Add Assignment.
+ *
+ * Adds the attributes of a different vec4 to the attributes of this vec4.
+ * @param other a vec4 whose values are added to this vec4's attributes
+ */
 void vec4::operator += (const vec4& other)
 {
     x += other.x; y += other.y; z += other.z; w += other.w;
 }
-/** Subtraction */
+
+/**
+ * @brief Subtraction Assignment.
+ *
+ * Subtracts the attributes of a different vec4 from the attributes of this vec4.
+ * @param other a vec4 whose values are subtracted from this vec4's attributes
+ */
 void vec4::operator -= (const vec4& other)
 {
     x -= other.x; y -= other.y; z -= other.z; w -= other.w;
 }
-/** Skalierung mit einem Skalar */
+
+/**
+ * @brief Scalar Scaling.
+ *
+ * Scales the attributes linearly by multiplying them with a scalar factor.
+ * @param factor scalar which in multiplied to all attributes
+ */
 void vec4::operator *= (float factor)
 {
     x *= factor; y *= factor; z *= factor; w *= factor;
 }
-/** Invertierte Skalierung durch Division mit einem Skalar */
-void vec4::operator /= (float factor)
-{
-    x /= factor; y /= factor; z /= factor; w /= factor;
-}
-///@}
 
 /**
- * @brief Länge des Vektors.
+ * @brief Scalar Divison.
  *
- * Berechnet die zweite Vektornorm (aka die Länge) des Vektors.
+ * Scales the attributes linearly by multiplying them with a scalar factor. The factor is the inverse of the given parameter
+ * @param dividend scalar that all attributes are divided by
+ */
+void vec4::operator /= (float dividend)
+{
+    float factor{ 1.f/dividend };
+    x *= factor; y *= factor; z *= factor; w *= factor;
+}
+
+// ----------------------------------------------
+// (Other) Methods
+
+/**
+ * @brief Vector Length.
+ *
+ * Calculates the second vector norm, i.e., the Euclidian norm or the vector length. This is the square root all the sum of the squares of all attributes.
  */
 float vec4::norm() const
 {
@@ -89,21 +105,21 @@ float vec4::norm() const
 }
 
 /**
- * @brief Einheitsvektor.
+ * @brief Unit Vector.
  *
- * Gibt den Vektor zurück, der in die selbe Richtung zeigt, aber eine Länge von 1 hat. Das wird durch das Dividieren mit der Länge des Vektors erreicht.
+ * Returns the a vector of length = 1, pointing in the same direction as this vec4. This is done by dividing each attribute by the length of the vector.
  */
 vec4 vec4::unit() const
 {
-    float l { norm() };
-    return vec4(x/l, y/l, z/l, w/l);
+    float l{ 1.f / norm() };
+    return vec4(x*l, y*l, z*l, w*l);
 }
 
 
 /**
- * @brief Konvertierung zu std::array.
+ * @brief Conversion to `std::array`.
  *
- * Erstellt einen 4-dimensionalen std::array, welcher die Koordinaten des Vec4 enthält.
+ * Returns a four-dimensional `std::array` containing the four attributes in the order of x, y, z, w.
  */
 std::array<float, vec4::size> vec4::array() const
 {
@@ -111,34 +127,22 @@ std::array<float, vec4::size> vec4::array() const
 }
 
 // ----------------------------------------------
-// Namespace Methoden
+// Namespace Methods
 
 /**
- * @brief Vektor Addition `+`.
+ * @brief Vector Operatoren.
  *
- * Die Summe von zwei Vektoren. Wegen des Kommutativgesetzes ist die Reihenfolge von `u` und `v` egal
+ * Implementation of the binary operators (`+`,`-`,`*`,`/`)  for vec4, which follow the standard math rules
  */
+///@{
 vec4 vml::operator + (const vec4& u, const vec4& v)
 {
     return vec4(u.x + v.x, u.y + v.y, u.z + v.z, u.w + v.w);
 }
-
-/**
- * @brief Vector Subtraktion `-`.
- *
- * Die Differenz von zwei Vektoren oder Punkten. Der resultierende Vektor entspricht dem Vektor zwischen den Punkten `u` und `v` (Spitze minus Anfang).
- */
 vec4 vml::operator - (const vec4& u, const vec4& v)
 {
     return vec4(u.x - v.x, u.y - v.y, u.z - v.z, u.w + v.w);
 }
-
-///@{
-/**
- * @brief Multiplikation mit Skalar.
- *
- * Die Skalierung eines Vektors `v` um einen `factor`. Die die Koordinaten des Vektors werden alle mit `factor` multipliziert.
- */
 vec4 vml::operator * (const vec4& v, float factor)
 {
     return vec4(factor * v.x, factor * v.y, factor * v.z, factor * v.w);
@@ -147,34 +151,27 @@ vec4 vml::operator * (float factor, const vec4& v)
 {
     return vec4(factor * v.x, factor * v.y, factor * v.z, factor * v.w);
 }
-///@{
-
-/**
- * @brief Division mit Skalar.
- *
- * Die Division eines Vektors `v` und eines Skalars `divident` entspricht der Multiplikation mit dem Kehrwert von `divident`. Diese Operation ist allerdings nicht kommutativ.
- */
-vec4 vml::operator / (const vec4& v, float divident)
+vec4 vml::operator / (const vec4& v, float dividend)
 {
-    return vec4(v.x / divident, v.y / divident, v.z / divident, v.w / divident);
+    float factor{ 1.f / dividend };
+    return vec4(factor * v.x, factor * v.y, factor * v.z, factor * v.w);
 }
+///@}
 
 /**
- * @brief Abstand zwischen zwei Punkten.
+ * @brief Vector Distance.
  *
- * Die Länge der Differenz aus zwei Vektoren.
- * Die Reihenfolgen von u und v spielt keine Rolle.
+ * Calculates the distance between two vectors. This is the length of the vector between u and v. The order of u and v does not matter.
  */
 float vml::distance(const vec4& u, const vec4& v)
 {
-    vec4 d{u - v};
-    return d.norm();
+    return (u - v).norm();
 }
 
 /**
- * @brief Skalarprodukt (dot product).
+ * @brief Dot Product.
  *
- * Berechnet das Skaraprodukt von zwei Vektoren `u` und `v`.
+ * Calculates the dot product of u and v.
  */
 float vml::dot(const vec4& u, const vec4& v)
 {
--- vml/vec4.h
+++ vml/vec4.h
@@ -8,54 +8,66 @@
 namespace vml {
 
 /**
- * @brief Vierdimensionaler Vektor.
+ * @brief 4D vector.
  *
- * Vec2 ist die vml Implementierung eines 2D-Vektors. Die API ist inspiriert von [GLSL](https://www.khronos.org/opengl/wiki/Data_Type_(GLSL)).
- * `vec4 = (x,y,z,w)`
+ * vec4 is the vml:: implementation of a four dimensional vector object. The API is designed to be similar to GLSL: `vec4 = (x,y,z,w)`.
+ * @seealso https://www.khronos.org/opengl/wiki/Data_Type_(GLSL)
  */
 struct vec4
 {
-    /// Dimension des Vektor
-    static const int size { 4 };
+    /// vector dimenisions
+    static const int size{ 4 };
 
-    ///@{
-    /// Die Koordinaten
-    float x;
-    float y;
-    float z;
-    float w;
-    ///@}
-    
-    // Konstruktoren
+    // Attributes
+    /**
+     * @brief The Coordinates. namez
+     *
+     * The coordinates are stored as named members of the vec4-struct to allow for convient member call via the `.`-operator.
+     */
+    float x, y, z, w;
+
+    // Constructors
     vec4(float, float, float, float);
     vec4(float);
-    vec4(std::array<float, size>);
     vec4();
-    
-    // Methoden
+
+    /**
+     * @brief Temaplated Array Constructor.
+     *
+     * Enables initializasion of a vec4 by passing an instance of a type that support the subscription operator, e.g. std::vector or std::array. This enable easy conversion from other C++libraries to vml-compatible data.
+     */
+    template<typename ArrayType>
+    vec4(const ArrayType& data) :
+    vec4(data[0], data[1], data[2], data[3])
+    {}
+
+    // Methods
     float norm() const;
     vec4 unit() const;
     std::array<float, size> array() const;
-    
-    // Operatoren
+
+    // subscription
+    /// @Todo: subscription, to allow for array style member access, member should be stored sequentially, so no flow control is needed.
+
+    // Operators
     vec4 operator -() const;
     void operator += (const vec4&);
     void operator -= (const vec4&);
     void operator *= (float);
     void operator /= (float);
-    
+
     // Debugging
     friend std::ostream& operator << (std::ostream&, const vec4&);
 };
 
 // ----------------------------------------------
-// Namespace Methoden
+// Namespace Methods
 
 vec4 operator + (const vec4& u, const vec4& v);
 vec4 operator - (const vec4& u, const vec4& v);
 vec4 operator * (const vec4& v, float factor);
 vec4 operator * (float factor, const vec4& v);
-vec4 operator / (const vec4& v, float divident);
+vec4 operator / (const vec4& v, float dividend);
 
 float distance(const vec4& u, const vec4& v);
 float dot(const vec4& u, const vec4& v);
